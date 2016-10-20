07cc4fc546fed91ea1117b732a9764b0eb9d82d4
Pat Wangrungarun <phatograph@gmail.com>
2016-10-20 15:55:18 +0700

    Basic router

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, Link } from 'react-router'
 
-let App = React.createClass({
-  render() {
-    return (
-      <h1>Hello!</h1>
-    );
-  }
-});
+const App = () => (
+  <div>
+    <h1>Wanikani</h1>
+    <ul role="nav">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+  </div>
+)
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h2>Whoops</h2>
+    <p>Sorry but {location.pathname} didn’t match any pages</p>
+  </div>
+)
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+  </Router>,
+  document.querySelector('#app')
+);
