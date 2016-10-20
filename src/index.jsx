71a6a8d40d4a0dd79870f441cd96b18212adcd8d
Pat Wangrungarun <phatograph@gmail.com>
2016-10-20 17:57:48 +0700

    Fetch Wanikani API

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
+import fetchJsonp from 'fetch-jsonp'
 
 const App = React.createClass({
   render() {
@@ -19,6 +20,9 @@ const App = React.createClass({
 
 const Home = React.createClass({
   componentDidMount() {
+    fetchJsonp('https://www.wanikani.com/api/user/8a026e69d462dd088b40b12b99437328/level-progression?callback=callback')
+    .then(response => response.json())
+    .then(json => console.log(json))
   },
   render() {
     return (
