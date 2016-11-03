c6a575726c4382465259537de1d43040395249f7
Pat Wangrungarun <phatograph@gmail.com>
2016-11-03 22:50:48 +0700

    Add server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express     = require('express');
+var path        = require('path');
+var app         = express();
+var static_path = path.join(__dirname, 'build');
+var port        = 4000;
+
+app.use(express.static(static_path));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {
+    root: static_path
+  });
+});
+
+app.listen(process.env.PORT || port, function (err) {
+  if (err) { console.log(err) };
+  console.log(`Listening at localhost:${port}`);
+});
